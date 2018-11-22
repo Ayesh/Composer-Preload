eb7e8e581c94753a9780a10b32689f607f134456
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2018-11-22 20:17:07 +0400

    Add no-status-check opton to preload command

--- src/Command/PreloadCommand.php
+++ src/Command/PreloadCommand.php
@@ -9,6 +9,7 @@ use Ayesh\ComposerPreload\PreloadWriter;
 use Composer\Command\BaseCommand;
 use Composer\IO\IOInterface;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class PreloadCommand extends BaseCommand {
@@ -17,7 +18,22 @@ class PreloadCommand extends BaseCommand {
 
   protected function configure() {
     $this->setName('preload');
-    $this->setDescription('Preloads the source files to PHP OPCache to speed up execution.');
+    $this->setDescription('Preloads the source files to PHP OPCache to speed up execution.')
+    ->setDefinition(array(
+      new InputOption('no-status-check', null, InputOption::VALUE_NONE, 'Do not include Opcache status checks in the generated file (useful if you want to combine multiple files).'),
+    ))
+    ->setHelp(
+      <<<HELP
+Composer Preload plugin adds this "preload" command, so you can generate a PHP file at 'vendor/preload.php' containing a list of 
+PHP files to load into opcache when called. This can significantly speed up your PHP applications if used correctly. 
+
+Use the --no-status-check option to generate the file without additional opcache 
+status checks. This can be useful if you want to include the 'vendor/preload.php' 
+within another script, so these checks redundent. This will override the 
+extra.preload.no-checks directive if used in the composer.json file.
+
+HELP
+    );
   }
 
   protected function execute(InputInterface $input, OutputInterface $output) {
@@ -32,9 +48,14 @@ class PreloadCommand extends BaseCommand {
       throw new \InvalidArgumentException('"preload" configuration is invalid.');
     }
 
-    $this->setConfig($extra['preload']);
+    $this->setConfig($extra['preload'], $input);
     $list = $this->generatePreload();
     $writer = new PreloadWriter($list);
+
+    if ($this->config['no-status-check']) {
+      $writer->setStatusCheck(false);
+    }
+
     $writer->write();
 
     $io = $this->getIO();
@@ -42,15 +63,18 @@ class PreloadCommand extends BaseCommand {
     $io->writeError(sprintf('Preload script contains %d files.', $writer->getCount()), true, IOInterface::VERBOSE);
   }
 
-  private function setConfig(array $config): void {
+  private function setConfig(array $config, InputInterface $input): void {
     $this->config = $config;
+
+    if ($input->getOption('no-status-check')) {
+      $this->config['no-status-check'] = true;
+    }
   }
 
   private function generatePreload(): PreloadList {
     $generator = new PreloadGenerator();
 
     $this->validateConfiguration();
-
 
     foreach ($this->config['paths'] as $path) {
       $generator->addPath($path);
@@ -77,6 +101,19 @@ class PreloadCommand extends BaseCommand {
             \gettype($path)
           ));
         }
+      }
+    }
+
+    $force_bool = ['no-status-check' => false];
+    foreach ($force_bool as $item => $default_value) {
+      if (!isset($this->config[$item])) {
+        $this->config[$item] = $default_value;
+      }
+
+      if (!\is_bool($this->config[$item])) {
+        throw new \InvalidArgumentException(sprintf('"%s" must be boolean value. %s given.',
+          'extra.preload.' . $item,
+          \gettype($this->config[$item])));
       }
     }
   }
