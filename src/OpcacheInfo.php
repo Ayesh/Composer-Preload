f53978ec415281cd15d49078c497a8bbe4e706d2
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2018-11-25 01:41:07 +0400

    Add stub for OpcacheInfo class

---
+++ src/OpcacheInfo.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Ayesh\ComposerPreload;
+
+
+use Ayesh\ComposerPreload\PreloadList;
+
+class OpcacheInfo {
+
+  /**
+   * @var PreloadList
+   */
+  private $preload_list;
+
+  private function __construct() {
+
+  }
+}
